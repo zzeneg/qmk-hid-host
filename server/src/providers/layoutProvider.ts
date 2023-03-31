f1a7b365fa77e2d6f9ad99e39783b72efba529e3
Evgenii Vilkov <zzeneg@gmail.com>
2023-04-01 01:23:33 +0200

    configurable layouts

--- server/src/providers/layoutProvider.ts
+++ server/src/providers/layoutProvider.ts
@@ -1,14 +1,6 @@
 import { DataType } from '../dataTypes';
 import { Provider } from './providerBase';
-
-enum Layout {
-  en = 0,
-  ru,
-}
-
-function isLayout(x: string): x is keyof typeof Layout {
-  return Object.keys(Layout).includes(x);
-}
+import config from './../config';
 
 class LayoutProvider extends Provider {
   constructor() {
@@ -17,8 +9,8 @@ class LayoutProvider extends Provider {
 
   override onAdapterDataPush = (data: unknown): void => {
     super.onAdapterDataPush(data);
-    if (typeof data === 'string' && isLayout(data)) {
-      this.pushDataFn([Layout[data]]);
+    if (typeof data === 'string' && config.layouts.includes(data)) {
+      this.pushDataFn([config.layouts.indexOf(data)]);
     } else {
       this.logUnsupportedAdapterData(data);
     }
