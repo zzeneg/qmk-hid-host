7143f0d271444e7b79bc384822c0b46abe262ea2
Evgenii Vilkov <zzeneg@gmail.com>
2023-08-01 00:50:31 +0200

    fix issues with media provider

--- src/providers/media.rs
+++ src/providers/media.rs
@@ -2,8 +2,7 @@ use async_std::task::block_on;
 use tokio::sync::{broadcast, mpsc};
 
 use windows::{
-    core::Error,
-    Foundation::TypedEventHandler,
+    Foundation::{EventRegistrationToken, TypedEventHandler},
     Media::Control::{GlobalSystemMediaTransportControlsSession, GlobalSystemMediaTransportControlsSessionManager},
 };
 
@@ -11,18 +10,43 @@ use crate::data_type::DataType;
 
 use super::_base::Provider;
 
-async fn get_media_session() -> Result<GlobalSystemMediaTransportControlsSession, Error> {
-    let mp = GlobalSystemMediaTransportControlsSessionManager::RequestAsync()?.await?;
-    let session = mp.GetCurrentSession();
-    return session;
+fn get_manager() -> GlobalSystemMediaTransportControlsSessionManager {
+    let manager = block_on(GlobalSystemMediaTransportControlsSessionManager::RequestAsync().unwrap());
+    return manager.unwrap();
 }
 
-async fn get_media_data(media_session: &GlobalSystemMediaTransportControlsSession) -> Result<(String, String), Error> {
-    let media_properties = media_session.TryGetMediaPropertiesAsync()?.await?;
+fn handle_session(session: &GlobalSystemMediaTransportControlsSession, data_sender: &mpsc::Sender<Vec<u8>>) -> EventRegistrationToken {
+    let (artist, title) = get_media_data(session);
+    send_data(DataType::MediaArtist, &artist, &data_sender);
+    send_data(DataType::MediaTitle, &title, &data_sender);
+    let mut synced_artist = artist;
+    let mut synced_title = title;
+
+    let data_sender = data_sender.clone();
+    let session_handler = &TypedEventHandler::new(move |_session: &Option<GlobalSystemMediaTransportControlsSession>, _| {
+        let (artist, title) = get_media_data(_session.as_ref().unwrap());
+        if synced_artist != artist {
+            synced_artist = artist;
+            send_data(DataType::MediaArtist, &synced_artist, &data_sender);
+        }
+
+        if synced_title != title {
+            synced_title = title;
+            send_data(DataType::MediaTitle, &synced_title, &data_sender);
+        }
+
+        Ok(())
+    });
+
+    return session.MediaPropertiesChanged(session_handler).unwrap();
+}
+
+fn get_media_data(session: &GlobalSystemMediaTransportControlsSession) -> (String, String) {
+    let media_properties = block_on(session.TryGetMediaPropertiesAsync().unwrap()).unwrap();
     let artist = media_properties.Artist().unwrap().to_string();
     let title = media_properties.Title().unwrap().to_string();
 
-    return Ok((artist, title));
+    return (artist, title);
 }
 
 fn send_data(data_type: DataType, value: &String, data_sender: &mpsc::Sender<Vec<u8>>) {
@@ -56,48 +80,36 @@ impl Provider for MediaProvider {
         let connected_sender = self.connected_sender.clone();
         std::thread::spawn(move || {
             let mut connected_receiver = connected_sender.subscribe();
-            let mut synced_artist = "".to_string();
-            let mut synced_title = "".to_string();
+            let mut session_token: Option<EventRegistrationToken> = None;
 
-            if let Ok(media_session) = block_on(get_media_session()) {
-                if let Ok((artist, title)) = block_on(get_media_data(&media_session)) {
-                    synced_artist = artist;
-                    send_data(DataType::MediaArtist, &synced_artist, &data_sender);
-                    synced_title = title;
-                    send_data(DataType::MediaTitle, &synced_title, &data_sender);
-                }
+            let manager = get_manager();
 
-                let handler = &TypedEventHandler::new(move |_session, _| {
-                    if let Some(session) = _session {
-                        if let Ok((artist, title)) = block_on(get_media_data(session)) {
-                            if synced_artist != artist {
-                                synced_artist = artist;
-                                send_data(DataType::MediaArtist, &synced_artist, &data_sender);
-                            }
-
-                            if synced_title != title {
-                                synced_title = title;
-                                send_data(DataType::MediaTitle, &synced_title, &data_sender);
-                            }
-                        }
-                    }
-
-                    return Ok(());
-                });
-                let token = media_session.MediaPropertiesChanged(handler).unwrap();
-
-                loop {
-                    if !connected_receiver.try_recv().unwrap_or(true) {
-                        break;
-                    }
-
-                    std::thread::sleep(std::time::Duration::from_millis(100));
-                }
-
-                media_session
-                    .RemoveMediaPropertiesChanged(token)
-                    .unwrap_or_else(|e| tracing::error!("{}", e));
+            if let Ok(session) = manager.GetCurrentSession() {
+                session_token = Some(handle_session(&session, &data_sender));
             }
+
+            let manager_handler = TypedEventHandler::new(move |_manager: &Option<GlobalSystemMediaTransportControlsSessionManager>, _| {
+                if let Ok(session) = _manager.as_ref().unwrap().GetCurrentSession() {
+                    if let Some(token) = session_token {
+                        session.RemoveMediaPropertiesChanged(token).unwrap();
+                    }
+                    session_token = Some(handle_session(&session, &data_sender));
+                }
+
+                Ok(())
+            });
+
+            let manager_token = manager.CurrentSessionChanged(&manager_handler).unwrap();
+
+            loop {
+                if !connected_receiver.try_recv().unwrap_or(true) {
+                    break;
+                }
+
+                std::thread::sleep(std::time::Duration::from_millis(100));
+            }
+
+            manager.RemoveCurrentSessionChanged(manager_token).unwrap();
 
             tracing::info!("Media Provider stopped");
         });
